57f6421df53608902b8ca4f58b88cd8dc36777b5
Arijit Chowdhury <143516210+student-Arijit@users.noreply.github.com>
2025-07-22 13:43:20 +0000

    background add

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
 import streamlit as st
 
-st.write("We are working on it.")
+page_bg_img = """
+<style>
+.stApp {
+    background-image: url("https://mrwallpaper.com/images/hd/hd-solar-flare-8rxanecj76jxibxm.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    position: relative;
+}
+
+.stApp::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(211, 211, 211, 0.3); /* white translucent overlay */
+    z-index: 0;
+}
+
+.stApp > * {
+    position: relative;
+    z-index: 1;
+}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.write("Hello")
